bcd36dbead0a04f8e69c4918d3a9f261627ad0f5
Ryota Murakami <dojce1048@gmail.com>
2024-05-01 07:59:45 +0900

    add protected route

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import { SignOutButton } from '@clerk/nextjs'
+import { auth, clerkClient } from '@clerk/nextjs/server'
+import React from 'react'
+
+const page = async () => {
+  const { userId } = auth().protect()
+
+  const user = await clerkClient.users.getUser(userId)
+  if (!user) return null
+
+  return (
+    <div>
+      dashboard: {user.id} <SignOutButton />
+    </div>
+  )
+}
+
+export default page
