230970c44e4cfbf18e88db80a7e6de3edfedf5f2
Ryota Murakami <dojce1048@gmail.com>
2024-05-06 22:17:37 +0900

    feat: enhance project setup with Prisma and dependency updates
     Add Prisma client and Prisma Studio to dependencies for ORM support.
     Update types/node to version 20.12.8 for improved type definitions.
     Introduce new scripts for Prisma Studio in

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,17 @@
 import { auth, clerkClient } from '@clerk/nextjs/server'
 import React from 'react'
 
-const page = async () => {
+const Page = async () => {
   const { userId } = auth().protect()
 
   const user = await clerkClient.users.getUser(userId)
   if (!user) return null
 
-  return <div className="">dashboard: {user.id} </div>
+  return (
+    <>
+      <div>dashboard: {user.id}</div>
+    </>
+  )
 }
 
-export default page
+export default Page
