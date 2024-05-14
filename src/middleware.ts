b6345c5baeefdc9bfea076e07b5dbf2585152dce
Ryota Murakami <dojce1048@gmail.com>
2024-05-15 08:47:26 +0900

    featwebhooks Add Svix webhook support and simplify middleware config
    - Added svix dependency to package.json for handling webhooks.
    - Created a new webhook route handler in srcappapiwebhooksroute.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,7 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware({
-  afterSignInUrl: '/dashboard',
-  afterSignUpUrl: '/dashboard',
-})
+export default clerkMiddleware()
 
 export const config = {
-  matcher: ['/((?!.+.[w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 }
