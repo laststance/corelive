1dae3c522f3f3e08206e5ce57cfef0a388f33e75
Ryota Murakami <dojce1048@gmail.com>
2024-05-16 07:49:01 +0900

    Update app layout description and remove viewport meta tag
    - Updated the project description in srcapplayout.tsx to correctly reflect the project name Unfarely instead of Corelive.
    - Removed the viewport meta tag from the

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Unfarely',
-  description: 'Corelive convert your leafy tasks to solid experiences.',
+  description: 'Unfarely convert your leafy tasks to solid experiences.',
 }
 
 export interface LayoutProps {
@@ -15,9 +15,6 @@ const RootLayout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <ClerkProvider>
       <html lang="en">
-        <head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </head>
         <body>{children}</body>
       </html>
     </ClerkProvider>
