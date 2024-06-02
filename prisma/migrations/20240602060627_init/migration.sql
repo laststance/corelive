4a7af6bc4bb002137f045eec3ee2d6afee30be36
Ryota Murakami <dojce1048@gmail.com>
2024-06-02 15:08:05 +0900

    Refactor database schema and migrations
    - Removed migration for making email optional in User table.
    - Renamed Act table to Task and updated related foreign keys.
    - Added Genre table with a relation to Task.
    - Updated initial migration to reflect new

--- prisma/migrations/20240602060627_init/migration.sql
+++ prisma/migrations/20240602060627_init/migration.sql
@@ -1,13 +1,24 @@
 -- CreateTable
-CREATE TABLE "Act" (
+CREATE TABLE "Task" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "title" VARCHAR(255) NOT NULL,
     "accomplished" BOOLEAN NOT NULL DEFAULT false,
     "authorId" INTEGER NOT NULL,
+    "genreId" INTEGER NOT NULL,
 
-    CONSTRAINT "Act_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Genre" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Genre_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -15,6 +26,8 @@ CREATE TABLE "Profile" (
     "id" SERIAL NOT NULL,
     "bio" TEXT,
     "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
 );
@@ -23,11 +36,16 @@ CREATE TABLE "Profile" (
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "clerkId" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
+    "email" TEXT,
     "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Genre_name_key" ON "Genre"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
@@ -39,7 +57,10 @@ CREATE UNIQUE INDEX "User_clerkId_key" ON "User"("clerkId");
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
-ALTER TABLE "Act" ADD CONSTRAINT "Act_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Task" ADD CONSTRAINT "Task_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_genreId_fkey" FOREIGN KEY ("genreId") REFERENCES "Genre"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
