a5b6ae53ebe4cf46a80b2a14ee89241333632aa1
Ryota Murakami <dojce1048@gmail.com>
2024-06-08 20:12:41 +0900

    featSimpleEditor: Add context menu with Completed option
    - Removed unused useRef import and selectAllText function.
    - Integrated use-context-menu to provide a context menu in the SimpleEditor component.
    -

--- src/components/SimpleEditor.tsx
+++ src/components/SimpleEditor.tsx
@@ -1,31 +1,60 @@
 'use client'
 
-import React, { useRef, type ComponentProps } from 'react'
+import React, { type ComponentProps } from 'react'
 import { toast } from 'sonner'
+import { ContextMenuItem, useContextMenu } from 'use-context-menu'
 
 import { cn } from '@/lib/utils'
+function taskCompleted() {
+  const textarea = document.getElementById(
+    'SimpleEditor',
+  )! as HTMLTextAreaElement
+  if (textarea.value === '') return
+
+  const cursorPosition = textarea.selectionStart
+  const textBeforeCursor = textarea.value.substring(0, cursorPosition)
+  const linesBeforeCursor = textBeforeCursor.split('\n')
+  const lineNumber = linesBeforeCursor.length
+  const currentLineContent = linesBeforeCursor[lineNumber - 1]!
+
+  // Calculate the start and end positions of the current line
+  const startPos = textBeforeCursor.lastIndexOf('\n') + 1
+  const endPos = startPos + currentLineContent.length
+
+  // Select the current line content
+  textarea.setSelectionRange(startPos, endPos)
+  textarea.focus() // Focus the textarea to show the selection
+
+  // Optional: Highlight the selection for a brief moment
+  setTimeout(() => {
+    textarea.setSelectionRange(cursorPosition, cursorPosition) // Reset selection to cursor position
+  }, 2000)
+  toast.success('Task completed')
+}
 
 export const SimpleEditor: React.FC<ComponentProps<'textarea'>> = ({
   className,
   ...rest
 }) => {
-  const textareaRef = useRef<HTMLTextAreaElement>(null)
+  const { contextMenu, onContextMenu } = useContextMenu(
+    <>
+      <ContextMenuItem onSelect={taskCompleted}>Completed</ContextMenuItem>
+    </>,
+  )
 
-  const selectAllText = () => {
-    toast.success('Text selected')
-  }
   return (
     <>
       <textarea
         {...rest}
-        ref={textareaRef}
+        id="SimpleEditor"
+        onContextMenu={onContextMenu}
         placeholder="Write your task step by step here..."
         className={cn(
           'textarea textarea-bordered textarea-lg h-full w-full max-w-xs',
           className,
         )}
       ></textarea>
-      <button onClick={selectAllText}>Select all</button>
+      {contextMenu}
     </>
   )
 }
