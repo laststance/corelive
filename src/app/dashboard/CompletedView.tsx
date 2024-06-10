220beed97d6a0f84ba3efa1fd9963a0f43269c86
Ryota Murakami <dojce1048@gmail.com>
2024-06-11 08:48:01 +0900

    Refactor CompletedView component for improved layout and readability
    - Removed unnecessary timeline classes and simplified the list structure.
    - Updated list items to use flexbox for better alignment and spacing.
    - Retained the SVG icon and adjusted its placement within the list items

--- src/app/dashboard/CompletedView.tsx
+++ src/app/dashboard/CompletedView.tsx
@@ -12,26 +12,22 @@ export const CompletedView: React.FC<Props> = () => {
   return (
     <>
       <h2 className="text-primary-content">Completed</h2>
-      <ul className="timeline timeline-vertical pr-[25%] text-primary-content">
+      <ul className="text-primary-content">
         {completed.map((item, i) => (
-          <li key={i}>
-            <div className="timeline-middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <div className="timeline-end timeline-box text-balance bg-neutral-content">
-              {item}
-            </div>
+          <li key={i} className="flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <div className="text-balance">{item}</div>
           </li>
         ))}
       </ul>
