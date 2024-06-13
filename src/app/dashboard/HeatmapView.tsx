873b2e411d23cff0014d5675e4c93c91f6adebec
Ryota Murakami <dojce1048@gmail.com>
2024-06-13 23:19:36 +0900

    Refactor HeatmapView to use useIsFirstRender hook
    - Added import for useIsFirstRender hook.
    - Modified HeatmapView component to initialize CalHeatmap only on the first render.
    - Ensured CalHeatmap is painted

--- src/app/dashboard/HeatmapView.tsx
+++ src/app/dashboard/HeatmapView.tsx
@@ -4,12 +4,17 @@
 import CalHeatmap from 'cal-heatmap'
 // @ts-ignore Could not find a declaration file for module 'cal-heatmap/plugins/Tooltip'.
 import Tooltip from 'cal-heatmap/plugins/Tooltip'
+
 import 'cal-heatmap/cal-heatmap.css'
+import { useIsFirstRender } from '@/hooks/useIsFirstRender'
 
 export default function HeatmapView() {
-  // @ts-ignore This expression is not constructable. Type 'typeof import("/Users/ryota.murakami/unfarely/node_modules/.pnpm/@types+cal-heatmap@3.5.39/node_modules/@types/cal-heatmap/index")' has no construct signatures.
-  const cal = new CalHeatmap()
-  cal.paint({ theme: 'dark' }, [[Tooltip]])
+  const firstRender = useIsFirstRender()
+  if (firstRender) {
+    // @ts-ignore This expression is not constructable. Type 'typeof import("/Users/ryota.murakami/unfarely/node_modules/.pnpm/@types+cal-heatmap@3.5.39/node_modules/@types/cal-heatmap/index")' has no construct signatures.
+    const cal = new CalHeatmap()
+    cal.paint({ theme: 'dark' }, [[Tooltip]])
+  }
 
   return <div id="cal-heatmap"></div>
 }
