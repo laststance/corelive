29cfc9103499871f5fba07d2a26f9ef7483b9698
Ryota Murakami <dojce1048@gmail.com>
2024-06-16 19:03:26 +0900

    Remove plate-components.json and clean up Tailwind config
    - Deleted plate-components.json which included schema style tailwind and alias configurations.
    - Removed custom keyframes and animations for accordion in tailwind.config.ts.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,22 +14,6 @@ const config: Config = {
       xl: '1280px', // 	@media (min-width: 1280px) { ... }
       '2xl': '1536px', // 	@media (min-width: 1536px) { ... }
     },
-    extend: {
-      keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
-        },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-      },
-    },
   },
   daisyui: {
     themes: [
