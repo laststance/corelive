cf93c73de26111bbc963bc481507e60c1315bf87
Ryota Murakami <dojce1048@gmail.com>
2024-07-07 11:41:55 +0900

    refactor: Remove redundant prose-slate classes from dashboard sections
    - Removed prose-slate class from the second and third sections in page.tsx to simplify class usage.
    - Ensured consistent styling across sections by retaining

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -36,10 +36,10 @@ const Page = async () => {
       <section className="h-[49vh]">
         <EditorView />
       </section>
-      <section className="prose prose-xl prose-slate grid h-[49vh] place-content-center rounded-md border border-neutral-content p-4">
+      <section className="prose prose-xl grid h-[49vh] place-content-center rounded-md border border-neutral-content p-4">
         <h1>{count} EXP</h1>
       </section>
-      <section className="prose prose-xl prose-slate flex h-[48vh] flex-col items-center overflow-scroll rounded-md bg-neutral-content p-4">
+      <section className="prose prose-xl flex h-[48vh] flex-col items-center overflow-scroll rounded-md bg-neutral-content p-4">
         <CompletedView completedTasks={completedTasks} />
       </section>
       <section className="grid h-[48vh] place-content-center rounded-md border-neutral-content p-4">
