3ddf518467539f3d50152569364fec68be51c5ff
Ryota Murakami <dojce1048@gmail.com>
2024-07-20 23:13:30 +0900

    Enhance NewCategoryDialog UI
    - Changed the heading from <h3> to <h1> for better accessibility and SEO.
    - Added a Create button with a success style inside a modal-action div for improved user interaction.

--- src/app/dashboard/NewCategoryDialog.tsx
+++ src/app/dashboard/NewCategoryDialog.tsx
@@ -6,8 +6,11 @@ export const NewCategoryDialog: React.FC<Props> = () => {
   return (
     <dialog id="new_category_modal" className="modal">
       <div className="modal-box">
-        <h3 className="text-lg font-bold">New Category</h3>
+        <h1 className="text-lg font-bold">New Category</h1>
         <p className="py-4">Press ESC key or click outside to close</p>
+        <div className="modal-action">
+          <button className="btn btn-success">Create</button>
+        </div>
       </div>
       <form method="dialog" className="modal-backdrop">
         <button>close</button>
