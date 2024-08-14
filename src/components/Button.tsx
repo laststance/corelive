af2154def3dbf27f5d3ea90da97064486f21117d
Ryota Murakami <dojce1048@gmail.com>
2024-08-14 22:22:36 +0900

    Refactor Button component to use namespace for Props type
    - Moved Props type definition into a Button namespace.
    - Updated the Button component to use Button.Props for type annotations.
    - Added ESLint directive to

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,21 @@
+/* eslint-disable no-redeclare */
 import { cn } from '@/lib/utils'
 
-type Props = {
-  className?: string
-  pending?: boolean
-  type?: 'button' | 'submit' | 'reset'
-  children: React.ReactNode
-} & React.ComponentProps<'button'>
-
-export const Button: React.FC<Props> = ({
+export namespace Button {
+  export type Props = {
+    className?: string
+    pending?: boolean
+    type?: 'button' | 'submit' | 'reset'
+    children: React.ReactNode
+  } & React.ComponentProps<'button'>
+}
+export function Button({
   children,
   className,
   pending,
   type = 'button',
   ...props
-}) => {
+}: Button.Props) {
   return (
     <button
       type={type}
